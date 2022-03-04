f039f27f703ce011a00955a37d7f42dc101a5278
github username <github email address>
2022-03-04 10:01:11 -0500

    first commit

---
+++ store/views.py
@@ -0,0 +1,115 @@
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models.aggregates import Count 
+from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter , OrderingFilter
+from rest_framework.mixins import CreateModelMixin ,RetrieveModelMixin , DestroyModelMixin , UpdateModelMixin
+from rest_framework.permissions import IsAuthenticated , AllowAny
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet , GenericViewSet
+from rest_framework import status
+from .models import OrderItem, Product , Collection, Review , Cart , CartItem , Customer
+from .filters import ProductFilter 
+from .pagination import DefaultPagination
+from .serializers import ProductSerializer , CollectionSerializer , ReviewSerializer ,CartSerializer , AddCartItemSerializer , \
+CartItemSerializer , UpdateCartItemSerializer , CustomerSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend,SearchFilter,OrderingFilter]
+    filterset_class = ProductFilter
+    pagination_class = DefaultPagination
+    search_fields = ['title','description']
+    ordering_fields = ['unit_price','last_update']
+
+     
+    def get_serializer_class(self):
+        return ProductSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
+            return Response({"error":"Product Cannot Be Deleted Because Its associated with an Order Item"})
+        return super().destroy(request, *args, **kwargs)
+
+
+class CollectionViewSet(ModelViewSet):
+    queryset = Collection.objects.annotate(products_count=Count('products')).all()
+    serializer_class = CollectionSerializer
+
+
+    def delete(self,request,pk) :
+        collection = get_object_or_404(Collection , pk = pk)
+        if collection.products.count() > 0 :
+            return Response({"error":"Collection  Cannot Be Deleted Because It Includes One Or More Products"}
+            ,status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT) 
+
+
+class ReviewViewSet(ModelViewSet):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+
+    def get_serializer_context(self):
+        return {'product_id':self.kwargs['product_pk']}
+
+
+class CartViewSet(CreateModelMixin,
+                  RetrieveModelMixin,
+                  DestroyModelMixin,
+                  GenericViewSet):
+    queryset = Cart.objects.prefetch_related('items__product').all()
+    serializer_class = CartSerializer 
+
+
+class CartItemViewSet(ModelViewSet):
+
+    http_method_names = ['get', 'post', 'patch','delete']
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AddCartItemSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateCartItemSerializer
+        return CartItemSerializer
+    
+    def get_serializer_context(self):
+        return {'cart_id':self.kwargs['cart_pk']} 
+
+    def get_queryset(self): 
+        return CartItem.objects \
+        .filter(cart_id=self.kwargs['cart_pk']) \
+        .select_related('product')
+
+
+class CustomerViewSet(CreateModelMixin,RetrieveModelMixin,UpdateModelMixin,GenericViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        return [IsAuthenticated]
+
+
+    @action(detail=False,methods=['GET','PUT'])
+    def me(self,request):
+        (customer,created) = Customer.objects.get_or_create(user_id=request.user.id)
+        if request.method == 'GET':
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data) 
+        elif request.method == 'PUT':
+            serializer = CustomerSerializer(customer,data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+        return Response(serializer.data) 
+
+
+
+
+
